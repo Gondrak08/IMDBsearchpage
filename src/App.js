0d6e2372742af291aa7d825a740d011ce86a9d39
Gondrak08 <vitor.alecrim.p@gmail.com>
2020-01-20 11:59:03 -0300

    beta

--- src/App.js
+++ src/App.js
@@ -2,17 +2,12 @@ import React from 'react';
 import Routes from './Routes'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-import Search from './pages/search/index'
-
-import Index from './pages/main/index'
 import Header from './components/Header'
 
 function App() {
   return (
     <> 
       <Header/>
-      {/* <Search/> */}
       <Routes/>
     </>
   );
--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter,  Switch, Route} from 'react-router-dom'
 import Main from './pages/main'
 import Product from './pages/product'
-import Search from './pages/search'
+
 
 const Routes = ()=>(
     <BrowserRouter>
